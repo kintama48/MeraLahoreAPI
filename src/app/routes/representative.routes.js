ba8cecb8a28fb8d2a46f1e0dca00c5e42d2234b7
kintama48 <rebelschaos@gmail.com>
2023-04-14 22:38:16 +0500

    api with routes

--- src/app/routes/representative.routes.js
+++ src/app/routes/representative.routes.js
@@ -1,11 +1,11 @@
-module.exports = app => {
-    const {
-        createRepresentative,
-        fetchAllRepresentatives,
-        deleteRepresentative,
-        updateRepresentative,
-    } = require('../controllers/representative.controller');
+const {
+    createRepresentative,
+    fetchAllRepresentatives,
+    deleteRepresentative,
+    updateRepresentative,
+} = require('../controllers/representative.controller');
 
+module.exports = app => {
     app.post('/representatives', createRepresentative);
     app.get('/representatives', fetchAllRepresentatives);
     app.delete('/representatives/:id', deleteRepresentative);
