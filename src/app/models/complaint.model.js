9c908911741c05ca8917ccef4a9dc5c84ec73bc2
Abdullah Baig <rebelschaos@gmail.com>
2023-05-18 17:31:12 +0500

    Update complaint.model.js

--- src/app/models/complaint.model.js
+++ src/app/models/complaint.model.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const complaintSchema = new Schema({
-    complainantName: { type: String, required: true, },
-    description: { type: String},
-    telephone: { type: String, required: true, },
-    email: { type: String, required: true, },
-    img: { type: String},
+    complainantName: { type: String },
+    description: { type: String, required: true},
+    telephone: { type: String },
+    email: { type: String },
+    img: { type: String, required: true},
     status: { type: String, required: true, enum: ['pending', 'in-progress', 'resolved', 'rejected'], default: 'pending'},
     ccId: { type: Object, required: true },
 }, {
